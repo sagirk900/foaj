091156d3f008f3368d30a6d8f1e3cbfc027782bc
sagir khan <sagirk900@gmail.com>
2025-03-03 20:31:16 +0300

    new changes

--- companies.js
+++ companies.js
@@ -9,6 +9,20 @@ function Direction() {
     var smallcomp = document.querySelector('#smallcomp');
     var smallcont = document.querySelector('#smallcont'); 
 
+    var comhead = document.querySelector('.compheaderleft h3');
+    var comheadrightfirst = document.querySelectorAll('.comheaderrightfirstright div');
+    var comheadrightsecond = document.querySelector('.comheaderrightsecondright div');
+
+    
+    var compcontentfirst = document.querySelector(".compcontentcenterrow1col2content p");
+    var compcontentsecond = document.querySelector(".compcontentcenterrow2col2content p");
+    var compcontentthird = document.querySelector(".compcontentcenterrow3col2content p");
+    var compcontentfour = document.querySelector(".compcontentcenterrow4col2content p");
+    var compcontentfive = document.querySelector(".compcontentcenterrow5col2content p");
+    var compcontentsix = document.querySelector(".compcontentcenterrow6col2content p");
+    var compcontentseven = document.querySelector(".compcontentcenterrow7col2content p");
+    var compcontentten = document.querySelector(".compcontentcenterrow10col2content p");
+
     var tooglebuttonfooter = document.querySelector('.footercpyrighttopright #toggleDirections');
     var footfoaj = document.querySelector("#footfoaj");
     var footproj = document.querySelector("#footproj");
@@ -31,6 +45,32 @@ function Direction() {
             smallcomp.textContent="شركات";
             smallcont.textContent="اتصال";
 
+            comhead.textContent="مجموعة الاتصالات المتكاملة";
+            comheadrightfirst.forEach((ele,index)=>{
+                if(index===0)
+                {
+                    ele.textContent="الرائدة"
+                }
+                else if(index===1)
+                {
+                    ele.textContent="شركات"
+                }
+                
+            });
+            comheadrightsecond.textContent="المهنيين";
+
+
+
+            compcontentfirst.textContent ="شركة دعاية وإعلان متكاملة، حاصلة على عدّة جوائز محلية وعالمية، تقدم حلول إعلانية من الفكرة والتنفيذ وحتى النشر والإطلاق وقياس الأثر."; 
+            compcontentsecond.textContent="شركة دعاية وإعلان متخصصة في تطوير الحلول الإبداعية، تقدّم الاستراتيجيات واﻟﻤحتوى والأفكار والتصاميم الإبداعية التي تعتني بهويّة اﻟﻤشروع وتعبّر عن قيمه.";
+            compcontentthird.textContent="وكالة دعاية وإعلان تملك الخبرة في الحملات التسويقية وتنفيذها وإدارتها، تقدم حزم دعائية متكاملة من محتوى وتصاميم إبداعية، عملت على العديد من الحملات لجهات كبرى في القطاعين الحكومي والخاص.";
+            compcontentfour.textContent="وكالة تسويق وإعًلام تصنع الحملات الاتصالية من الاستراتيجية والتخطيط، وصولا للتنفيذ والإطلاق، عملت على تنفيذ العديد من اﻟﻤشاريع الحكومية لأكثر من جهة.";
+            compcontentfive.textContent="شركة رائدة في التسويق الرقمي، نفذت أكثر من 350 حملة إعلامية وأدارت التواجد الرقمي لعدد كبير من اﻟﻤشاريع، ساهمت في نجاح العديد من اﻟﻤشاريع الحكومية الكبرى.";
+            compcontentsix.textContent="شركة متخصصة في تقديم خدمات التواصل الرقمي للقطاع الخاص من خلال إدارة حسابات التواصل الاجتماﻋﻲ وصناعة اﻟﻤحتوى الإبداﻋﻲ، وإطلاق الحملات الترويجية الرقمية.";
+            compcontentseven.textContent="شركة متخصصة في العلاقات العامة الإعلامية على اﻟﻤستوى اﻟﻤحلي والدولي، تهتم في تخطيط وتنفيذ الحملات الإعلامية، والتدريب على الظهور الإعلامي واللقاءات الإعلامية، والتقارير الإعلامية، والأخبار الصحفية.";
+            compcontentten.textContent="شركة متخصصة في تخطيط وتنفيذ الحملات الإعلانية المدفوعة من خلال مختلف المنصات الإعلانية.";
+
+
             footfoaj.textContent="فواج";
             footproj.textContent="المشاريع";
             footcomp.textContent="شركات";
@@ -50,6 +90,32 @@ function Direction() {
             smallcomp.textContent="Companies";
             smallcont.textContent="Contact";
             
+
+            comhead.textContent="Integrated Communications Group";
+            comheadrightfirst.forEach((ele,index)=>{
+                if(index===0)
+                {
+                    ele.textContent="Leading"
+                }
+                else if(index===1)
+                {
+                    ele.textContent="Companies"
+                }
+                
+            });
+            comheadrightsecond.textContent="Professionals";  
+
+
+            compcontentfirst.textContent="An integrated creative house that earned several local and international awards. Habbar offers creative comprehensive advertising solutions from strategy and conceptualization to execution, publication, launch, and impact measurement.";
+            compcontentsecond.textContent="A creative house specialized in developing creative solutions, providing strategies, content, ideas, and creative designs that reflect the identity and values of each unique project.";
+            compcontentthird.textContent="An advertising agency with expertise in development of marketing campaigns, execution, and management. Pavo offers comprehensive advertising packages of creative concepts, content and design. It has worked on multiple campaigns for major entities in both the government and private sectors.";
+            compcontentfour.textContent="An advertising and marketing company that crafts communication campaigns from strategy and planning to execution and launch. Akwan worked on many government projects for multiple entities.";
+            compcontentfive.textContent="A leading digital marketing company that has executed over 350 media campaigns and managed the digital presence of numerous projects, contributing to the success of major government initiatives.";
+            compcontentsix.textContent="A company specialized in providing creative digital communication services for the private and governmental sector. Biio manages social media accounts, creating creative content, and launching digital campaigns.";
+            compcontentseven.textContent="March is specialized in public relations on both local and international levels, focusing on planning and executing media campaigns, media appearance training, interviews, media reports, and press releases.";
+            compcontentten.textContent="A company specialized in planning and executing paid advertising campaigns through various advertising platforms.";
+
+
             footfoaj.textContent="Foaj";
             footproj.textContent="Projects";
             footcomp.textContent="Companies";
--- foaj.js
+++ foaj.js
@@ -10,8 +10,21 @@ function Direction() {
     var smallcont = document.querySelector('#smallcont'); 
 
     var foajabouth3 = document.querySelector(".foajaboutleftcontenttop h3");
-    var foajaboutp = document.querySelector(".foajaboutleftcontentbottom p")
+    var foajaboutp = document.querySelector(".foajaboutleftcontentbottom p");
 
+    var foajcountfirst = document.querySelectorAll(".foajcountcontent1bottom div");
+    var foajcountsecond = document.querySelectorAll(".foajcountcontent2bottom div");
+    var foajcountthird = document.querySelector(".foajcountcontent3bottom div");
+    var foajcountfour = document.querySelector(".foajcountcontent4bottom div");
+
+    let heading1 = document.querySelector(".foajdatasectiontoprow1content h1");
+    let heading2 = document.querySelector(".foajdatasectiontoprow2content h1");
+    let heading3 = document.querySelector(".foajdatasectiontoprow3content h1");
+    let heading4 = document.querySelector(".foajdatasectiontoprow4content h1");
+    let databottom = document.querySelector(".foajdatasectionbottomcontent p");
+
+
+    
     var tooglebuttonfooter = document.querySelector('.footercpyrighttopright #toggleDirections');
     var footfoaj = document.querySelector("#footfoaj");
     var footproj = document.querySelector("#footproj");
@@ -39,6 +52,40 @@ function Direction() {
             foajaboutp.textContent="أول وأكبر اتحاد لشركات الاتصال والتسويق الرائدة على مســـتوى المنطقة، في منظومة اتـصالّية متكاملة تجمع بين قوة التواصل والتخطيط والإبداع.";
 
 
+            foajcountfirst.forEach((ele,index)=>{
+                if(index===0)
+                {
+                    ele.textContent="السوق"
+                }
+                else if(index===1)
+                {
+                    ele.textContent="الشركات"
+                }
+                else if(index===2)
+                {
+                    ele.textContent="الرائدة"
+                }
+            });
+            foajcountsecond.forEach((ele,index)=>{
+                if(index===0)
+                {
+                    ele.textContent="الشراكات"
+                }
+                else if(index===1)
+                {
+                    ele.textContent="الاستراتيجية"
+                }
+            });
+            foajcountthird.textContent="المهنيين";
+            foajcountfour.textContent="المشاريع";
+
+
+            heading1.innerHTML=`<span>01/</span> وجهة واحدة لإدارة محفظة المشروع`;
+            heading2.innerHTML=`<span>02/</span> خبرة استشارية واستراتيجية في قيادة المشاريع الكبرى`;
+            heading3.innerHTML=`<span>03/</span> سـجل غنـي بالنجاحات والإنجــازات`;
+            heading4.innerHTML=`<span>04/</span> قيــادات وطنّية ذات خــبرة وكـفاءة`;
+            databottom.textContent="إدارة المشاريع الكبرى وبناء استراتيجيات اتصالية متكاملة تتغلب على التحديات التي تواجهها، وتنفيذها عبر شــــركات اﻟﻤجموعة، لتحقيق الأهـــداف الاســتراتيجية وإحداث التأثير على الصعيدين المحلّي والدولي."
+
             footfoaj.textContent="فواج";
             footproj.textContent="المشاريع";
             footcomp.textContent="شركات";
@@ -61,7 +108,42 @@ function Direction() {
             foajabouth3.textContent="The Largest Communication Alliance in the Region";
             foajaboutp.textContent="The first and largest alliance of communication and marketing companies in the region creating an integrated ecosystem that merges communication, planning, and creativity.";
 
+            foajcountfirst.forEach((ele,index)=>{
+                if(index===0)
+                {
+                    ele.textContent="market"
+                }
+                else if(index===1)
+                {
+                    ele.textContent="Leading"
+                }
+                else if(index===2)
+                {
+                    ele.textContent="Companies"
+                }
+            });
+            foajcountsecond.forEach((ele,index)=>{
+                if(index===0)
+                {
+                    ele.textContent="Strategic"
+                }
+                else if(index===1)
+                {
+                    ele.textContent="Partnerships"
+                }
+            });
+            foajcountthird.textContent="Professionals";
+            foajcountfour.textContent="Projects";
             
+            heading1.innerHTML=`<span>01/</span> One destination for managing project portfolios.`;
+            heading2.innerHTML=`<span>02/</span> Consultation and strategy experience in leading major projects.`;
+            heading3.innerHTML=`<span>03/</span> Established track record of successes.`;
+            heading4.innerHTML=`<span>04/</span> Market leading local expertise and competencies.`;
+            databottom.textContent="Managing mega projects and building integrated communication strategies that are executed by group companies and strategic partners to overcome challenges and achieve goals to create impact on local and international scales"
+
+
+
+
             footfoaj.textContent="Foaj";
             footproj.textContent="Projects";
             footcomp.textContent="Companies";
